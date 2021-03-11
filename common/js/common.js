40e1a4e1ec0859e256a5a585c92bd589c31ee0f1
kimminyoung <arome0825@naver.com>
2021-03-11 16:36:53 +0900

    kim minyoung 03/11

--- common/js/common.js
+++ common/js/common.js
@@ -1,10 +1,10 @@
 $(function(){
     $('#gnb > li').mouseenter(function(){
-        $('#header #gnb > li .towD').slideDown();
+        $('#header #gnb > li .towD').stop().slideDown();
         $('#header .gnbBg').stop().slideDown();
     });
     $('#header .gnbBg').mouseleave(function(){
-        $('#header #gnb > li .towD').slideUp();
+        $('#header #gnb > li .towD').stop().slideUp();
         $('#header .gnbBg').stop().slideUp();
     });
 });
